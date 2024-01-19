b48df8e733bcbc91c00c038cd562f75ffc6c4757
Weronika E. Falinska <92479890+makikooni@users.noreply.github.com>
2024-01-19 23:02:06 +0000

    memeMaker_upload

---
+++ product_one/memeMaker/memeMaker/CaptionOption.swift
@@ -0,0 +1,13 @@
+//
+//  CaptionOption.swift
+//  memeMaker
+//
+//  Created by Weronika E. Falinska on 19/01/2024.
+//
+
+import Foundation
+
+struct CaptionOption{
+    var emoji = ""
+    var caption = ""
+}
